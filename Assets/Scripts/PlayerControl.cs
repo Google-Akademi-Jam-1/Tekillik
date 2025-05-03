6bff7f74bd39aed98f13115032925869db63b645
Turan <mehmetemir2run@gmail.com>
2025-05-03 20:51:07 +0300

    player controller, putted on player.

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "light")
+        {
+
+        }
+    }
 }
