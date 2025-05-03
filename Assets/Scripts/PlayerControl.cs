cd8438d61fbfbf487fe4d067448e5c2c73cadd5c
YavuzAvcii <yavperen@gmail.com>
2025-05-03 20:55:33 +0300

    started player controls

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,5 +2,58 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-   
+    [SerializeField]
+    float speed;
+
+    [SerializeField]
+    float jumpForce;
+
+    Rigidbody2D rb;
+    bool onGround = true;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void Update()
+    {
+        HandleControls();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.GetMask("platform"))
+        {
+            onGround = true;
+        }
+    }
+
+    void HandleControls()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Run(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Run(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+    void Jump()
+    {
+        if (onGround)
+        {
+            onGround = false;
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + jumpForce);
+        }
+    }
+
+    void Run(int direction)
+    {
+        rb.velocity = new Vector2(speed * direction, rb.velocity.y);
+    }
 }
