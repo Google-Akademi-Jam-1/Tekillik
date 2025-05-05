52be10f5f8e8475442f3b1f12073900cd49a7343
Turan <mehmetemir2run@gmail.com>
2025-05-05 20:51:16 +0300

    Intro added.

--- Assets/Scripts/IntroController.cs
+++ Assets/Scripts/IntroController.cs
@@ -3,28 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class IntroController : MonoBehaviour
 {
-    public float scrollSpeed = 30f; // Ne kadar hýzlý hareket edecek
-    public RectTransform textTransform;
+    [SerializeField]
+    private GameObject[] images;
 
-    private float endY = -20f;
+    public float waitDuration = 3f;
+    public float transitionDuration = .5f;
 
     public Button myButton;
 
     void Start()
     {
+        myButton.gameObject.SetActive(false);
         // Ardýndan MusicManager'dan çaðrý ekle
         myButton.onClick.AddListener(() => MusicManager.instance.NewLevel("Office1"));
+        StartCoroutine(SlideShow());
     }
 
-    void Update()
+    IEnumerator SlideShow()
     {
-        if (textTransform.anchoredPosition.y < endY)
-        {
-            textTransform.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
-        }
+        images[0].GetComponent<CanvasGroup>().DOFade(1f, transitionDuration);
+        yield return new WaitForSeconds(waitDuration);
+        images[0].GetComponent<CanvasGroup>().DOFade(0f, transitionDuration);
+        images[1].GetComponent<CanvasGroup>().DOFade(1f, transitionDuration);
+        yield return new WaitForSeconds(waitDuration);
+        images[1].GetComponent<CanvasGroup>().DOFade(0f, transitionDuration);
+        images[2].GetComponent<CanvasGroup>().DOFade(1f, transitionDuration);
+        yield return new WaitForSeconds(waitDuration);
+        myButton.gameObject.SetActive(true);
+        myButton.GetComponent<CanvasGroup>().DOFade(1f, transitionDuration);
+
+
     }
 
 
